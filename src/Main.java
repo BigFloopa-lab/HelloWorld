b29b2cec29da62e181c8fd0a5035ba1724d37413
User <git@github.com:BigFloopa-lab/HelloWorld.git>
2025-09-15 23:26:07 +0300

    commit instant

--- src/Main.java
+++ src/Main.java
@@ -84,5 +84,6 @@ System.out.println("Маша теперь получает " + mashasNewSalary +
 System.out.println("Денис теперь получает " + denisNewSalary + " рублей. Годовой доход вырос на " + denisDifference + " рублей");
 System.out.println("Кристина теперь получает " + kristinaNewSalary + " рублей. Годовой доход вырос на " + kristinaDifference + " рублей");
     }
+
 }
 
