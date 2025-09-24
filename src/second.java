81f9298947cb6c418c55486252ab1fd03f937259
User <git@github.com:BigFloopa-lab/HelloWorld.git>
2025-09-24 21:26:32 +0300

    rebuild hw on 23.09 by 24.09

--- src/second.java
+++ src/second.java
@@ -1,6 +1,7 @@
 public class second {
 public static void main (String[] args){
 
+    //4 задание
     byte childAge = 5;
     if (childAge >= 2 && childAge <= 6) {
         System.out.println("Если возраст человека равен " + childAge + " то ему нужно ходить в детский сад.");
@@ -10,6 +11,21 @@ public static void main (String[] args){
         System.out.println("Если возраст человека равен " + childAge + " то ему нужно ходить в университет.");
     } else {
         System.out.println("Если возраст человека равен " + childAge + " то ему нужно ходить на работу.");
+    }
+
+    //6 задание
+    System.out.println("---6 задание---");
+    byte allPositions = 120;
+    byte seatingPositions = 60;
+    int standingPositions = allPositions - seatingPositions;
+    int reqiredPositions = 34;
+
+    if (reqiredPositions < seatingPositions) {
+        System.out.println("В вагоне есть сидячие места");
+    } else if (reqiredPositions < seatingPositions + standingPositions) {
+        System.out.println("В вагоне есть только стоячие места");
+    } else {
+        System.out.println("Вагон полностью заполнен");
     }
 }
 }
